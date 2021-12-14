538cbe9d2f07c9860b8f27358741960d478fd3c0
xia <313008430@qq.com>
2021-12-14 10:20:24 +0800

    代码测试发布master‘

--- bin/js/bundle.js
+++ bin/js/bundle.js
@@ -527,9 +527,9 @@
     BuildType2["online"] = "online";
     BuildType2["debug"] = "debug";
   })(BuildType || (BuildType = {}));
-  console.log("test");
+  console.log("online");
   var baseUrl = "http://game.ahd168.com:3000";
-  switch ("test") {
+  switch ("online") {
     case BuildType.debug:
       baseUrl = "//192.168.101.50:3000";
       break;
@@ -3456,7 +3456,7 @@
     }
     getBuildType() {
       let buildType = null;
-      switch ("test") {
+      switch ("online") {
         case BuildType.debug:
           buildType = 3;
           break;
@@ -5197,7 +5197,7 @@
         Laya.Stat.show();
       Laya.alertGlobalError(true);
       Laya.stage.bgColor = "#ffffff";
-      BuildType.debug == "test" && GameConfig.stat && Laya.Stat.show();
+      BuildType.debug == "online" && GameConfig.stat && Laya.Stat.show();
       Laya.ResourceVersion.enable("version.json", Laya.Handler.create(this, this.onVersionLoaded), Laya.ResourceVersion.FILENAME_VERSION);
     }
     onVersionLoaded() {
