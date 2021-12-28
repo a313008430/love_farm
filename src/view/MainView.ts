e503a923286d318ab8e37baf78dad26a1ac89983
xia <313008430@qq.com>
2021-12-28 20:46:05 +0800

    备份

--- src/view/MainView.ts
+++ src/view/MainView.ts
@@ -1484,7 +1484,6 @@ export default class MainView extends Core.gameScript {
         const nickname = d?.nickname,
             pedId = d?.dogId,
             avatar = friendData?.avatar || d?.avatar;
-        console.log(avatar);
 
         const topBox = this.orderBox.parent as Laya.Box,
             moneyBox = topBox.getChildByName("money_box") as Laya.Box,
