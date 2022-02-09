2d819d848fffa399b7b7ad6afe01bf65c94c1d9d
xia <313008430@qq.com>
2022-02-09 10:38:55 +0800

    隐藏客服按钮

---
+++ release/web/index-8c6dc07f44.js
@@ -0,0 +1,1 @@
+window.screenOrientation="sensor_landscape",loadLib("libs/min/laya-4fc7637454.core.min.js"),loadLib("libs/min/laya-a72326a604.ani.min.js"),loadLib("libs/min/laya-453646c875.html.min.js"),loadLib("libs/min/laya-b4568be5f8.ui.min.js"),loadLib("libs/min/bytebuffer-9637a546e0.min.js"),loadLib("js/bundle-4b7e72de77.js");
